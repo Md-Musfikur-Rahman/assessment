858a75e873a81ceed18a44395baa9c26c1b1e4f2
Md Musfikur Rahman <107528767+Md-Musfikur-Rahman@users.noreply.github.com>
2024-04-02 12:20:57 +0600

    Responsive desktop first approch.

--- components/CouponModal.tsx
+++ components/CouponModal.tsx
@@ -1,6 +1,6 @@
 const CouponModal = ({ onClose }: any) => {
   return (
-    <div className=" bg-[#F6F6F6] rounded-full w-64 pl-6 py-[6px] pr-[6px] flex gap-6 items-center absolute -bottom-12">
+    <div className=" bg-[#F6F6F6] rounded-full w-64 pl-6 py-[6px] pr-[6px] flex gap-6 items-center lg:absolute -bottom-12">
       <div className="text-[#B9B9B9] text-[12px] font-extralight">
         Enter Coupon if Any
       </div>
